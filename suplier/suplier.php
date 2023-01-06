a1f0e011af4159eac99121985e5def696f3c633a
ayyub887 <ayyubngoding@gmail.com>
2023-01-06 12:38:39 +0700

    menambah icon

--- suplier/suplier.php
+++ suplier/suplier.php
@@ -40,7 +40,8 @@ $no = 1;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Page Suplier</title>
+    <link rel="website icon" href="../image/Customer.svg"/>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
